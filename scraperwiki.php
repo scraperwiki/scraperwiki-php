62aeafce338565226e349f1fa5c991b6369b7463
Zarino Zappia <mail@zarino.co.uk>
2014-03-20 13:49:43 +0000

    automatic whitespace tidying

--- scraperwiki.php
+++ scraperwiki.php
@@ -14,36 +14,35 @@ protected $db;
 
 public function __construct($db = 'sqlite:scraperwiki.sqlite') {
 	// connect
-	scraperwiki::_connect($db);	
+	scraperwiki::_connect($db);
 }
 
 
 // set up the db connection
 static function _connect($db = null) {
 	if(empty($db)) {
-		R::setup();	// for testing locally or where zero config is possible. Use like this: scraperwiki::_connect('');		
+		R::setup();	// for testing locally or where zero config is possible. Use like this: scraperwiki::_connect('');
 	} else {
-		R::setup($db);	
+		R::setup($db);
 	}
 }
 
 
 static function save($unique_keys = array(), $data, $table_name="swdata", $date = null) {
-   $ldata = $data;   
+   $ldata = $data;
    if (!is_null($date))
-      $ldata["date"] = $date; 
+      $ldata["date"] = $date;
    return scraperwiki::save_sqlite($unique_keys, $ldata, $table_name);
 }
 
 
 // A port of scraperwiki::save_sqlite from classic using RedbeanPHP
-
 static function	save_sqlite($unique_keys = array(), $data, $table_name='swdata') {
 
     if (count($data) == 0)
         return;
 
-	$table = R::dispense($table_name);	
+	$table = R::dispense($table_name);
 
      // convert special types
      foreach ($data as &$value) {
@@ -67,10 +66,10 @@ static function	save_sqlite($unique_keys = array(), $data, $table_name='swdata')
 
     // If this is the first row ever added, use this to create table and exit
 	if (!R::$redbean->tableExists($table_name)) {
-		
+
 		// Define unique keys when creating table
 		if(!empty($unique_keys)) {
- 			$table->setMeta("buildcommand.unique", array($unique_keys));					
+ 			$table->setMeta("buildcommand.unique", array($unique_keys));
 		}
 
 		R::store($table);
@@ -81,7 +80,7 @@ static function	save_sqlite($unique_keys = array(), $data, $table_name='swdata')
     if(!empty($unique_keys)) {
 
         $parameters['table_name'] = $table_name;
-        $parameters['keys'] = join(", ", array_keys($data)); 
+        $parameters['keys'] = join(", ", array_keys($data));
         $parameters['values'] = join(', ', array_fill(0, count($data), '?')); // adds the ? placeholder for values
 
         $sql = vsprintf('INSERT or REPLACE INTO %s (%s) VALUES (%s)', $parameters);
@@ -92,12 +91,13 @@ static function	save_sqlite($unique_keys = array(), $data, $table_name='swdata')
     } else {
 
 		// if table already exists and doesn't have unique keys, just add this row
-	    R::store($table);	                
+	    R::store($table);
 		return true;
-    }		
+    }
 
 
 }
+
 
 static function select($sql, $data = null) {
    if (is_null($data))
@@ -106,12 +106,13 @@ static function select($sql, $data = null) {
    return $result;
 }
 
-static function save_var($name, $value) {	
-   $vtype = gettype($value); 
+
+static function save_var($name, $value) {
+   $vtype = gettype($value);
    if (($vtype != "integer") && ($vtype != "string") && ($vtype != "double") && ($vtype != "NULL"))
-      print_r("*** object of type $vtype converted to string\n"); 
-   $data = array("name"=>$name, "value_blob"=>strval($value), "type"=>$vtype); 
-   scraperwiki::save_sqlite(array("name"), $data, "swvariables"); 
+      print_r("*** object of type $vtype converted to string\n");
+   $data = array("name"=>$name, "value_blob"=>strval($value), "type"=>$vtype);
+   scraperwiki::save_sqlite(array("name"), $data, "swvariables");
 }
 
 
@@ -121,19 +122,19 @@ static function get_var($name, $default=null) {
            ' name = ? ',array($name));
 
    if (!$data)
-      return $default; 
+      return $default;
 
    $svalue = $data->value_blob;
    $vtype = $data->type;
 
    if ($vtype == "integer")
-      return intval($svalue); 
+      return intval($svalue);
    if ($vtype == "double")
-      return floatval($svalue); 
+      return floatval($svalue);
    if ($vtype == "NULL")
       return null;
 
-   return $svalue; 
+   return $svalue;
 }
 
 
@@ -143,15 +144,13 @@ static function scrape($url) {
   curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
   curl_setopt($curl, CURLOPT_MAXREDIRS, 10);
   // disable SSL checking to match behaviour in Python/Ruby.
-  // ideally would be fixed by configuring curl to use a proper 
+  // ideally would be fixed by configuring curl to use a proper
   // reverse SSL proxy, and making our http proxy support that.
   curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
   $res = curl_exec($curl);
   curl_close($curl);
   return $res;
 }
-
-
 
 }
 
